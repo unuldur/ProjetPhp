14229707d3fa9a882bce6d344c26f2c8d76ec786
Julien <unuldur.seladon@laposte.net>
2015-11-20 19:47:10 +0100

    Initial commit

---
+++ Index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE HTML>
+<html>
+
+<head>
+
+    <head>
+        <title>Bootstrap template</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+<body>
+<div class="container">
+
+    <nav class="navbar navbar-inverse">
+        <div class="container-fluid">
+            <ul class="nav navbar-nav">
+                <li class="active"> <a href="#">Accueil</a> </li>
+                <li> <a href="#">Liens</a> </li>
+                <li> <a href="#">Témoignages</a> </li>
+                <li> <a href="#">Références</a> </li>
+            </ul>
+            <form class="navbar-form navbar-right inline-form">
+                <div class="form-group">
+                    <input type="search" class="input-sm form-control" placeholder="Recherche">
+                    <button type="submit" class="btn btn-primary btn-sm"><span class="glyphicon glyphicon-eye-open"></span> Chercher</button>
+                </div>
+            </form>
+        </div>
+    </nav>
+
+
+</body>
+</html>
