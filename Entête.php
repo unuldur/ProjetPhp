23ac6b81597e3319e187bafda67fd0a408a70394
Julien <unuldur.seladon@laposte.net>
2015-11-21 10:48:09 +0100

    Test

---
+++ Entête.php
@@ -0,0 +1,44 @@
+<!DOCTYPE HTML>
+    <head>
+        <title>Bootstrap template</title>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+        <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+        <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+        <!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <div class="container">
+            <header class="page-header text-center">
+                <h1>Mon jeu</h1>
+            </header>
+            <nav class="navbar navbar-inverse">
+                <div class="container-fluid">
+                    <ul class="nav navbar-nav">
+                        <li class="active"> <a href="#">Accueil</a> </li>
+                        <li class="dropdown">
+                            <a data-toggle="dropdown" href="#">Informations<b class="caret"></b></a>
+                            <ul class="dropdown-menu">
+                                <li><a href="#">Monstres</a></li>
+                                <li><a href="#">Armes</a></li>
+                                <li><a href="#">Compétences</a></li>
+                                <li class="divider"></li>
+                                <li><a href="#">Ce qui est prévu</a></li>
+                            </ul>
+                        </li>
+                    </ul>
+                    <form class="navbar-form navbar-right form-inline">
+                        <div class="form-group">
+                            <button type="submit" class="btn btn-info btn-sm">Administrateur</button>
+                        </div>
+                    </form>
+                </div>
+            </nav>
+        </div>
+        <script src="js/jquery.js"></script>
+        <script src="bootstrap/js/bootstrap.js "></script>
