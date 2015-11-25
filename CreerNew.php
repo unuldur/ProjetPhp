841130b6fd2b3421808332e7d9da0e392384a6b2
Come de Montgolfier <comedm@hotmail.fr>
2015-11-25 17:30:54 +0100

    Rectification 1

---
+++ CreerNew.php
@@ -0,0 +1,13 @@
+<!DOCTYPE HTML>
+<html xmlns="http://www.w3.org/1999/html">
+<?php require_once "Entete.php";?>
+    <body>
+        <div class="container">
+            <?php
+                require_once "Debut.php";
+            ?>
+            
+        </div>
+        <?php require_once "Footer.php"; ?>
+    </body>
+</html>
---
+++ Footer.php
@@ -0,0 +1,9 @@
+<hr>
+<footer>
+    <div class="container">
+        <p class="text-muted">Site réalisé par Julien Defiolles et Côme de Montgolfier, tous droits réservés.</p>
+    </div>
+</footer>
+
+<script src="js/jquery.js"></script>
+<script src="bootstrap/js/bootstrap.js "></script>
